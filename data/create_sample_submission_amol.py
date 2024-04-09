0c201352a6bb298a0505d69c54515d29a2e385e3
Thomas Wood <thomas@fastdatascience.com>
2024-04-09 12:09:58 +0100

    placeholder for code for Amol

---
+++ data/create_sample_submission_amol.py
@@ -0,0 +1,128 @@
+'''
+MIT License
+
+Copyright (c) 2023 Ulster University (https://www.ulster.ac.uk).
+Project: Harmony (https://harmonydata.ac.uk)
+Maintainer: Thomas Wood (https://fastdatascience.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+'''
+
+import os
+import pandas as pd
+import sys
+import re
+import json
+
+# TODO: AMOL
+def extract_questions(pdf_plain_text: str, tables_as_dict: dict) -> str:
+    '''
+    Placeholder function for extracting questionnaire items from a PDF
+    '''
+    predictions = []
+    if len(tables_as_dict) > 0:
+        if len(tables_as_dict['pageTables']) == 1:
+            for table in tables_as_dict['pageTables'][0]:
+                for row in table:
+                    col = row[0].strip()
+                    if re.findall("[a-z]", col):
+                        predictions.append("\t" + col)
+    if len(predictions) > 0:
+        return "\n".join(predictions)
+    else:
+        for line in pdf_plain_text.split("\n"):
+            line = re.sub(r'\s+', ' ', line).strip()
+            line = re.sub(r'\n+', '', line)
+            line = re.sub(r'^\d+\.?', '', line).strip()
+            predictions.append("\t" + line)
+    return "\n".join(predictions)
+
+
+test_files = ["000_mfqchildselfreportshort.txt",
+"001_patienthealthquestionnaire.txt",
+"004_newyorklongitudinalstudybythom.txt",
+"005_beckdepressioninventorybdi.txt",
+"006_apadsm5severitymeasurefordepre.txt",
+"007_bhrcsselfreportedscared.txt",
+"008_gad7anxietyupdated0.txt",
+"009_validationoftheportugueseversi.txt",
+"011_bhrcsparentreportsocialaptitud.txt",
+"012_hrcw2protocolopsicoconfmenor18.txt",
+"013_hrcw2protocolopsicoadultos.txt",
+"014_testedeansiedadegad7dranadimme.txt",
+"015_hrcw1dom.txt",
+"016_hrcw2protocolodomiciliarmenor1.txt",
+"019_bhrcsparentreportsocialcohesio.txt",
+"021_sdqenglishukpt417single.txt",
+"023_hrcw1psico.txt",
+"024_bhrcsselfreportedmfq.txt",
+"026_sf36.txt",
+"030_bhrcsselfreportedwarwickedinbr.txt",
+"031_ghq12.txt",
+"032_bhrcsparentreportsdqchild.txt",
+"034_bhrcsparentreportsocialcohesio.txt",
+"037_hrcw0dom.txt",
+"039_selfmeasuresforlonelinessandin.txt",
+"045_mr0510201tb.txt",
+"046_borderlinepersonalityscreener.txt",
+"047_bhrcsparentreportsdqadult.txt",
+"055_beta_retirement.txt",
+"062_eoin_no_numbers.txt",
+"064_hrcw1psicoconf.txt"]
+
+COMMAND_LINE_PARAM = f"Usage: python create_sample_submission.py train|test"
+
+if len(sys.argv) < 2:
+    DATASET = "train"
+else:
+    DATASET = sys.argv[1]
+
+if DATASET != "train" and DATASET != "test":
+    print(COMMAND_LINE_PARAM)
+    exit()
+
+if DATASET == "test":
+    txt_files_to_process = test_files
+    output_file = "submission_amol.csv"
+else:
+    txt_files_to_process = os.listdir("preprocessed_text")
+    output_file = "train_predictions_amol.csv"
+all_json_files = set(os.listdir("preprocessed_tables"))
+
+ids = []
+predictions = []
+for txt_file in txt_files_to_process:
+    json_file = re.sub(r'txt$', 'json', txt_file)
+    with open("preprocessed_text/" + txt_file, "r") as f:
+        file_in_plain_text = f.read()
+    if json_file in all_json_files:
+        with open("preprocessed_tables/" + json_file, "r") as f:
+            file_in_json = f.read()
+        tables_as_dict = json.loads(file_in_json)
+    else:
+        tables_as_dict = {}
+    predictions.append(extract_questions(file_in_plain_text, tables_as_dict))
+    ids.append(txt_file)
+df = pd.DataFrame()
+df["ID"] = ids
+df["predict"] = predictions
+
+print ("Writing predictions to", output_file)
+df.to_csv(output_file, index=False)
---
+++ data/evaluate_train_results_amol.py
@@ -0,0 +1,87 @@
+'''
+MIT License
+
+Copyright (c) 2023 Ulster University (https://www.ulster.ac.uk).
+Project: Harmony (https://harmonydata.ac.uk)
+Maintainer: Thomas Wood (https://fastdatascience.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+'''
+
+import pandas as pd
+import os
+import re
+import numpy as np
+
+df = pd.read_csv("train_predictions_amol.csv")
+
+
+precisions = []
+recalls = []
+
+total_num_intersections = []
+total_num_predictions = []
+total_num_ground_truths = []
+
+for file_name in os.listdir("annotations/"):
+    ground_truth_file = f"annotations/{file_name}"
+    ground_truths = []
+    with open(ground_truth_file, "r", encoding="utf-8") as f:
+        for l in f:
+            l = re.sub(r'\n', '', l)
+            c = l.split("\t")
+            ground_truths.append(re.sub(r'\W', '', c[1]))
+    predictions = []
+
+    if file_name not in set(df.ID):
+        print("\tmissing", file_name)
+        continue
+    prediction = df[df.ID == file_name].predict.iloc[0]
+    for l in prediction.split("\n"):
+            l = re.sub(r'\n', '', l)
+            if l == "" or l == " ":
+                continue
+            c = l.split("\t")
+            predictions.append(re.sub(r'\W', '', c[1]))
+
+    size_of_intersection = len(set(ground_truths).intersection(set(predictions)))
+    if len(predictions) > 0:
+        precision = size_of_intersection / len(set(predictions))
+    else:
+        precision = 0
+    if len(ground_truths) > 0:
+        recall = size_of_intersection / len(set(ground_truths))
+    else:
+        recall = 0
+    print(f"Precision = {precision:.2f}, recall = {recall:.2f}\t{file_name}")
+
+    precisions.append(precision)
+    recalls.append(recall)
+
+    total_num_intersections.append(size_of_intersection)
+    total_num_predictions.append(len(set(predictions)))
+    total_num_ground_truths.append(len(set(ground_truths)))
+
+precision = np.mean(precisions)
+recall = np.mean(recalls)
+print(f"Mean precision = {precision:.2f}, mean recall = {recall:.2f}")
+precision = np.sum(total_num_intersections) / np.sum(total_num_predictions)
+recall = np.sum(total_num_intersections) / np.sum(total_num_ground_truths)
+print(f"\tPrecision over all instances = {precision:.2f}, recall over all instances = {recall:.2f}")
